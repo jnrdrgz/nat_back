55f00e076dd6a9b39e8a46c69ebb2e70bff2a290
leo <leo1104_tuc@hotmail.com>
2020-10-21 14:02:23 -0300

    falta ver setCicloActual

--- src/controllers/ciclo.js
+++ src/controllers/ciclo.js
@@ -10,6 +10,85 @@ exports.agregarCiclo = asyncHandler(async (req, res, next) => {
 
     const ciclo = await Ciclo.create(req.body);
     await ciclo.save()
-    
+
     res.status(200).json({ success: true, data: ciclo });
+})
+
+
+exports.getAllCiclos = asyncHandler(async (req, res, next) => {
+    let ciclos = await Ciclo.findAll({
+        attributes: [
+          "id",
+          "fechaInicio",
+          "fechaFin",
+          "numero",
+          "actual",
+          "estaEliminado",
+        ],
+        where: {
+            estaEliminado: false
+        }
+    });
+
+    return res.status(200).json({ success: true, data: ciclos });
+})
+
+
+
+exports.getCicloActual = asyncHandler(async (req, res, next) => {
+    let cicloActual = await Ciclo.findAll({
+        attributes: [
+          "id",
+          "fechaInicio",
+          "fechaFin",
+          "numero",
+          "actual",
+          "estaEliminado",
+        ],
+        where: {
+            actual: true
+        }
+    });
+
+    return res.status(200).json({ success: true, data: cicloActual });
+})
+
+
+
+exports.setCicloActual = asyncHandler(async (req, res, next) => {
+    let getActual = await Ciclo.findAll({
+        attributes: [
+          "id",
+          "fechaInicio",
+          "fechaFin",
+          "numero",
+          "actual",
+          "estaEliminado",
+        ],
+        where: {
+            actual: true
+        }
+    });
+
+    getActual.actual = false
+    getActual.save()
+
+
+
+    const setActual = await Ciclo.findByPk(req.body.id, {
+         attributes: [
+           "id",
+           "fechaInicio",
+           "fechaFin",
+           "numero",
+           "actual",
+           "estaEliminado",
+         ]
+     });
+
+     setActual.actual = true
+     setActual.save()
+
+     res.status(200).json({ success: true, data:{} });
+
 })
--- src/routes/ciclos.js
+++ src/routes/ciclos.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    agregarCiclo
+    agregarCiclo,
+    getCicloActual,
+    setCicloActual,
+    getAllCiclos
 } = require("../controllers/ciclo");
 
-//router.route("/").get(getCiclos);
+router.route("/").get(getAllCiclos);
 router.route("/agregar").post(agregarCiclo);
+router.route("/actual").get(getCicloActual);
+router.route("/setActual").put(setCicloActual);
+
 
 
 module.exports = router;
