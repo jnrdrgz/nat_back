3c9036e204c6555d3f1bab10e70c0bae99c004dd
MartinMiguelRR <martin97.rodri@gmail.com>
2020-10-29 05:57:56 -0300

    getPedidosAdeudados tumbero

--- src/controllers/pedidos.js
+++ src/controllers/pedidos.js
@@ -1,5 +1,10 @@
 const {Op} = require('sequelize')
-const {Pedido} = require('../sequelize')
+const {
+    Pedido,
+    PedidoCliente,
+    DetallePedido,
+} = require('../sequelize')
+    
 const asyncHandler = require("../middlewares/asyncHandler")
 
 exports.eliminarPedido = asyncHandler(async (req, res, next) => {
@@ -19,3 +24,50 @@ exports.eliminarPedido = asyncHandler(async (req, res, next) => {
 
     res.status(200).json({ success: true, data:{} });
 })
+
+exports.getPedidosAdeudados = asyncHandler(async (req, res, next) => {
+    console.log(req.body)
+
+    let pedidos = await PedidoCliente.findAll({
+        attributes: [
+            "id",
+            "montoSaldado",
+            "entregado",
+            "pagado"
+        ],
+        include: [
+            { model: Pedido, attributes: ["id", "fecha", "total", "cancelado"]},
+        ],
+        where: {
+            pagado: false,
+            entregado: true
+        }
+    });
+
+    return res.status(200).json({ success: true, data: pedidos });
+})
+
+/*
+exports.getPedidosAdeudados = asyncHandler(async (req, res, next) => {
+    console.log(req.body)
+
+    let pedidos = await Pedido.findAll({
+        attributes: [
+            "id",
+            "fecha",
+            "total",
+            "cancelado",
+        ],
+        include: [
+            { model: PedidoCliente, attributes: ["entregado", "pagado"]},
+            { model: DetallePedido, attributes: ["cantidad", "subtotal"]},
+        ],
+        where: {
+            pagado: false
+        }
+    });
+
+    return res.status(200).json({ success: true, data: pedidos });
+})
+
+*/
--- src/routes/pedidos.js
+++ src/routes/pedidos.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const {
     eliminarPedido,
+    getPedidosAdeudados,
 } = require("../controllers/pedidos");
 
 router.route("/eliminar").put(eliminarPedido);
+router.route("/traerPedidosAdeudados").get(getPedidosAdeudados);
 
 module.exports = router;
